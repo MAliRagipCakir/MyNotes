fead536d04bbdf8fba6addae4c6de5151d82a4f5
MAliRagipCakir <m-ar-cakir@hotmail.com>
2022-03-22 12:25:34 +0300

    list notes

--- my-notes-ui/src/App.js
+++ my-notes-ui/src/App.js
@@ -1,10 +1,28 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 import './App.css';
 
+const API_URL = "https://localhost:5001/";
+
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+
+    axios.get(API_URL + "api/Notes")
+      .then(function (response) {
+        setNotes(response.data);
+      });
+
+  }, []);
+
+
   return (
     <div className="App">
       <h1>My Notes</h1>
-      
+      <ul>
+        {notes.map((x, i) => <li key={i}>{x.title}</li>)}
+      </ul>
     </div>
   );
 }
