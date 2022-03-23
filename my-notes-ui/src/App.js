53fb249612b9824ce04850a30ff9faafbbf68ef1
MAliRagipCakir <m-ar-cakir@hotmail.com>
2022-03-23 12:30:58 +0300

    put post delete

--- my-notes-ui/src/App.js
+++ my-notes-ui/src/App.js
@@ -8,7 +8,7 @@ const API_URL = "https://localhost:5001/";
 
 function App() {
   const [notes, setNotes] = useState([]);
-  const [selectedNote, setSelectedNote] = useState({ id: 0, title: "", content: ""})
+  const [selectedNote, setSelectedNote] = useState({ id: 0, title: "", content: "" })
   useEffect(() => {
 
     axios.get(API_URL + "api/Notes")
@@ -21,7 +21,37 @@ function App() {
   }, []);
 
   const selectNote = function (note) {
-    setSelectedNote({...note});
+    setSelectedNote({ ...note });
+  };
+
+  const handleSubmit = function (event) {
+    event.preventDefault();
+
+    axios.put(API_URL + "api/Notes/" + selectedNote.id, selectedNote)
+      .then(function (response) {
+        const newNotes = [...notes];
+        const i = newNotes.findIndex(x => x.id == response.data.id);
+        newNotes[i] = response.data;
+        setNotes(newNotes);
+      });
+  };
+
+  const handleNewClick = function (event) {
+    axios.post(API_URL + "api/Notes", { title: "New Note", content: "" })
+      .then(function (response) {
+        const newNotes = [...notes];
+        newNotes.push(response.data);
+        setNotes(newNotes);
+        selectNote(response.data);
+      });
+  };
+
+  const handleDeleteClick = function (event) {
+    axios.delete(API_URL + "api/Notes/" + selectedNote.id)
+      .then(function (response) {
+        setNotes(notes.filter(x => x.id != selectedNote.id));
+        setSelectedNote({ id: 0, title: "", content: "" });
+      });
   };
 
   return (
@@ -31,26 +61,29 @@ function App() {
           <Col sm={4} lg={3}>
             <div className="d-flex">
               <h2 className="me-auto">My Notes</h2>
-              <Button variant="success">New</Button>
+              <Button variant="success" onClick={handleNewClick}>New</Button>
             </div>
             <ListGroup className="my-3">
               {notes.map((note, index) =>
-                <ListGroup.Item action active={note.id == selectedNote.id} onClick={() => selectNote(note)} >
+                <ListGroup.Item key={index} action active={note.id == selectedNote.id} onClick={() => selectNote(note)} >
                   {note.title}
                 </ListGroup.Item>
               )}
             </ListGroup>
           </Col>
-          {selectedNote &&
+          {selectedNote.id != 0 &&
             <Col sm={8} lg={9}>
-              <Form.Control type="text" placeholder="Title" value={selectedNote.title} onChange={(e) => setSelectedNote({ ...selectedNote, title: e.target.value})} />
+              <form onSubmit={handleSubmit}>
 
-              <Form.Control as="textarea" rows={9} placeholder="Content" className="mt-3" value={selectedNote.content} onChange={(e) => setSelectedNote({...selectedNote,content: e.target.value})} />
+                <Form.Control type="text" placeholder="Title" value={selectedNote.title} onChange={(e) => setSelectedNote({ ...selectedNote, title: e.target.value })} required />
 
-              <div className="mt-3 d-flex justify-content-end">
-                <Button variant="danger" className="me-2">Delete</Button>
-                <Button>Save</Button>
-              </div>
+                <Form.Control as="textarea" rows={9} placeholder="Content" className="mt-3" value={selectedNote.content} onChange={(e) => setSelectedNote({ ...selectedNote, content: e.target.value })} />
+
+                <div className="mt-3 d-flex justify-content-end">
+                  <Button type="button" variant="danger" className="me-2" onClick={handleDeleteClick}>Delete</Button>
+                  <Button type="submit">Save</Button>
+                </div>
+              </form>
             </Col>
           }
         </Row>
